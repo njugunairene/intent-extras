633b8bb267ad6cac216b433b3e5bedcd85e9f20a
ngujunairene <njokiirenenjuguna@gmail.com>
2019-07-01 22:16:58 +0300

    first commit

---
+++ app/src/main/java/com/example/intentextras/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.intentextras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText_name);
+        EditText editText = (EditText) findViewById(R.id.editText_age);
+        Button button = (Button) findViewById(R.id.btn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =editText.getText().toString();
+                int age =editText.getText().toString();
+
+                EditText displayText;
+                displayText.setText(name);
+
+            }
+        });
+    }
+
+}
