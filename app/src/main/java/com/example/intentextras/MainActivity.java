7b5b8f28ec839b106488071e5fbafaf1c21fbc29
ngujunairene <njokiirenenjuguna@gmail.com>
2019-07-03 23:13:27 +0300

    files

--- app/src/main/java/com/example/intentextras/MainActivity.java
+++ app/src/main/java/com/example/intentextras/MainActivity.java
@@ -7,28 +7,52 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etName;
+    EditText etAge;
+    Button btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        EditText editText = (EditText) findViewById(R.id.editText_name);
-        EditText editText = (EditText) findViewById(R.id.editText_age);
-        Button button = (Button) findViewById(R.id.btn);
-
-
-        button.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name =editText.getText().toString();
-                int age =editText.getText().toString();
-
-                EditText displayText;
-                displayText.setText(name);
-
+                etName = findViewById(R.id.etName);
+                etAge = findViewById(R.id.etAge);
+                Intent myIntent = new Intent(getBaseContext(),ReceiverActivity.class);
+                String name = etName.getText().toString();
+                String age = etAge.getText().toString();
+                myIntent.putExtra("etName",name);
+                myIntent.putExtra("etAge",age);
+                startActivity(myIntent);
             }
         });
+
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/intentextras/Receiver_Activity.java
+++ app/src/main/java/com/example/intentextras/Receiver_Activity.java
@@ -6,8 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Receiver_Activity extends AppCompatActivity {
+public class ReceiverActivity extends AppCompatActivity {
+    TextView tvName;
+    TextView tvAge;
+    String names;
+    String ages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,15 @@ public class Receiver_Activity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        tvName=findViewById(R.id.tvName);
+        tvAge=findViewById(R.id.tvAge);
+        Bundle extras = getIntent().getExtras();
+        if (extras!=null){
+            String name = extras.getString("etName");
+            String age = extras.getString("etAge");
+            tvName.setText(name);
+            tvAge.setText(age);
+        }
 
-}
+
+    }
